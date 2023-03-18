4d53d350dbfdfa94c1f27130f6f83a0d35c47234
Nicky Ngailo <nickyrabit@gmail.com>
2023-03-19 02:03:28 +0300

    create an account api is done

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import { generateAccount, SignatureType } from '@unique-nft/accounts';
 import express, {Express, Request, Response} from 'express';
+
+const bareUrl = "https://rest.unique.network/opal/v1";
+
 
 const app: Express = express();
 const port = 3000;
@@ -9,4 +13,23 @@ app.get('/', (req: Request, res: Response)=>{
 
 app.listen(port, ()=> {
 console.log(`[Server]: I am running at https://localhost:${port}`);
+});
+
+
+
+app.get('/create-account', async (req, res) => {
+    
+    const account = await generateAccount({
+      pairType: SignatureType.Sr25519,
+      meta: {
+        name: 'my_unique_account'
+      }
+    })
+    
+    console.log(account);
+ 
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(account));
+
+ 
 });
